7c6688ceef23b1f815405db8c7035398f9e0c4da
Ashim <ashimshrestha2384@gmail.com>
2024-11-28 19:47:03 +0545

    implementing multer

--- server/index.js
+++ server/index.js
@@ -7,8 +7,33 @@ const {
   getTokenByCitizenshipId,
   getTokenUriByCitizenshipId,
 } = require("./etherss");
+const multer = require("multer");
 
-app.get("/", async (req, res) => {
+//app.use(express.urlencoded({ extended: false }));
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    return cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    return cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+//const upload = multer({ dest: "uploads/" });
+const upload = multer({ storage });
+
+app.post(
+  "/file",
+  //  upload.single("citizenship"),
+  upload.fields([{ name: "front" }, { name: "back" }]),
+  (req, res) => {
+    console.log(req.body);
+    console.log(req.files);
+    res.send(req.files);
+  }
+);
+
+app.get("/connect", async (req, res) => {
   const result = await connect();
   res.json(result);
 });
